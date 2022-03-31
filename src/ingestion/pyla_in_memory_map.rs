edad413044b64f9cd92d9367808a6203365fb7d0
Sandeep Virdi <code.monkey.1982@gmail.com>
2022-03-30 21:48:10 -0700

    removed lifetime annotations from PylaEntry, added Bytes to represent the Entry

--- src/ingestion/pyla_in_memory_map.rs
+++ src/ingestion/pyla_in_memory_map.rs
@@ -1,16 +1,21 @@
-use crossbeam_skiplist::SkipMap;
-use crate::ingestion::PylaEntry;
 
-use super::PylaId;
-pub struct PylaInMemoryMap<'a> {
-  inner: SkipMap<PylaId, &'a [u8]>,
+use std::borrow::Borrow;
+
+use crate::ingestion::PylaEntry;
+use crossbeam_skiplist::{SkipMap};
+
+use super::{PylaId, pyla_internals::NotFound};
+pub struct PylaInMemoryMap {
+    inner: SkipMap<PylaId, PylaEntry>,
 }
 
-impl<'a> PylaInMemoryMap<'a> {
-    pub fn get(&self, pyla_entry: PylaId) -> Option<PylaEntry<'a>> {
-        match self.inner.get(&pyla_entry) {
-          Some(ref entry) => todo!(), 
-            None => None,
+impl PylaInMemoryMap {
+    pub fn get(&self, pyla_id: PylaId) -> Result<PylaEntry, NotFound> {
+        match self.inner.get(&pyla_id) {
+            Some(entry) => {
+                Ok(entry.value().clone())
+            },
+            None => Err(NotFound::new()),
         }
     }
 }
--- src/ingestion/pyla_internals.rs
+++ src/ingestion/pyla_internals.rs
@@ -1,8 +1,28 @@
-pub struct PylaEntry<'a> {
-  pub key: &'a [u8],
-  pub value: &'a [u8],
+use std::collections::btree_map::Values;
+
+use bytes::{Bytes, BytesMut, BufMut};
+
+#[derive(Debug, Clone)]
+pub struct PylaEntry {
+  pub underlying: Bytes
 }
 
+impl PylaEntry {
+  pub fn new(key: &[u8], value: &[u8]) -> PylaEntry {
+    let key_size = key.len() as u32;
+    let value_size = value.len() as u32;
+    let total_size = u64::BITS + key_size + u64::BITS + value_size; // keysize:actual_key:valuesize:actual_value
+    let mut buf =
+      BytesMut::with_capacity(total_size as usize);
+      // TODO - Endianess 
+
+    buf.put_u32(key_size);
+    buf.put_slice(key);
+    buf.put_u32(value_size);
+    buf.put_slice(value);
+    PylaEntry { underlying: buf.freeze() }
+  }    
+}
 
 /// PylaId represents a wrapper (new-type) around `u64` type.
 ///
@@ -18,4 +38,12 @@ impl PylaId {
   pub fn new(id: u64) -> PylaId {
     PylaId(id)
   }
+}
+
+pub struct NotFound();
+
+impl NotFound {
+    pub fn new() -> NotFound {
+      NotFound()
+    }
 }
